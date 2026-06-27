a4b3b5e4c033942c7b73827825fbfd73d3e7ee76
edilkgr <tvoy_email@gmail.com>
2026-06-27 16:34:35 +0600

    11111111122

--- bot_client.js
+++ bot_client.js
@@ -4,10 +4,9 @@ module.exports = function setupClientBot(bot, supabase) {
     
     // Приветствие и кнопка запуска Web App
     bot.start((ctx) => {
-        const cacheBuster = Date.now(); 
         ctx.reply('Добро пожаловать в ТамакKG! 🍔\nСамая быстрая доставка на Иссык-Куле.', 
             Markup.inlineKeyboard([
-                [Markup.button.webApp('🍕 Открыть меню', `https://edilkg.github.io/superkgapp/?v=${cacheBuster}`)] 
+                [Markup.button.webApp('🍕 Открыть меню', 'https://superkgapp.vercel.app/')] 
             ])
         );
     });
